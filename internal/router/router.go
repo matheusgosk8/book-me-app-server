c50f9c0ef19516b5a903f48c56feba936f2e40af
Wainer J F Ramos <jardelwainer@gmail.com>
2026-03-20 16:48:58 -0300

    feat: implementa login handler e rotas publicas

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,20 +3,24 @@ package router
 import (
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors" // <--- Adicione este import
 	"github.com/matheusgosk8/book-me-server/internal/middleware"
 	public "github.com/matheusgosk8/book-me-server/internal/router/public"
 )
 
 func Router(r *chi.Mux) {
 
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
 	r.Use(chimiddle.StripSlashes)
 	r.Use(middleware.LogRoute)
 
 	r.Mount("/public", public.PublicRouter())
-
-	//Routes
-	// 	r.Route("/public", func(router chi.Router) {
-	// 		router.Get("/status", routes.Status)
-	// 		router.Post("/register", routes.Register)
-	// 	})
 }
