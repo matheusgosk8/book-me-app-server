287bbb35618e76fe8304ec64cee491da823b0213
Wainer J F Ramos <jardelwainer@gmail.com>
2026-03-23 17:23:37 -0300

    integra autenticação redux com backend go e estrutura de roles

--- internal/handlers/login.go
+++ internal/handlers/login.go
@@ -3,22 +3,51 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"github.com/matheusgosk8/book-me-server/internal/utils"
+	log "github.com/sirupsen/logrus"
 )
+
+type LoginResponse struct {
+	Message string `json:"message"`
+	Token   string `json:"token"`
+	User    struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+		Role string `json:"role"`
+	} `json:"user"`
+}
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
-	response := map[string]interface{}{
-		"message": "Login realizado com sucesso!",
-		"user": map[string]interface{}{
-			"id":    1,
-			"name":  "Wâiner Jardel",
-			"email": "teste@teste.com",
-			"role": "prestador",
-		},
-		"token": "token-de-teste-123",
+
+	// Dados mockados para teste
+	userID := "1"
+	userName := "Wâiner Jardel"
+	userRole := "provider"
+
+	// Gerando o JWT real usando sua utilitária do .env
+	token, err := utils.GenerateJWT(userID)
+	if err != nil {
+		log.Errorf("Erro ao gerar JWT no Login: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Erro ao gerar token"})
+		return
 	}
-	
+
+	response := LoginResponse{
+		Message: "Login successful",
+		Token:   token,
+		User: struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Role string `json:"role"`
+		}{
+			ID:   1,
+			Name: userName,
+			Role: userRole,
+		},
+	}
+
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
 }
